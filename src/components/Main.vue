84aa051257dfcf5b089164a0fbf87cd388a186a0
Alethepata <alessia.patane@gmail.com>
2023-10-10 15:34:14 +0200

    [Add] main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,7 +5,141 @@ export default {
 </script>
 
 <template>
-  
+      <main>
+    <div class="card-container container flex">
+      <div class="card">
+        <div class="contenuto-card">
+          <div class="image-card">
+            <img class="first-img"  src="../assets/img/1.webp" alt="item">
+            <img class="second-img" src="../assets/img/1b.webp" alt="item">
+          </div>
+          <div class="discount">
+            <span class="discount-red">-50%</span>
+            <span class=" discount-green">Sostenibilità</span>
+          </div>
+          <div class="heart">
+            <span>&hearts;</span>
+          </div>
+        </div>
+        <div class="text">
+          <span>Levi's</span>
+          <span>RELAXED FIT TEE UNISEX</span>
+        </div>
+        <div class="prezzo">
+          <span>14,99 &euro;</span>
+          <span class="line-through">29,99 &euro;</span>
+        </div>
+      </div>
+      <div class="card">
+        <div class="contenuto-card">
+          <div class="image-card">
+            <img class="first-img"  src="../assets/img/2.webp" alt="item">
+            <img class="second-img" src="../assets/img/2b.webp" alt="item">
+          </div>
+          <div class="discount">
+            <span class="discount-red">-50%</span>
+          </div>
+          <div class="heart">
+            <span>&hearts;</span>
+          </div>
+        </div>
+        <div class="text">
+          <span>Guess</span>
+          <span>ROSES TEE</span>
+        </div>
+        <div class="prezzo">
+          <span>20,99 &euro;</span>
+          <span class="line-through">29,99 &euro;</span>
+        </div>
+      </div>
+      <div class="card">
+        <div class="contenuto-card">
+          <div class="image-card">
+            <img class="first-img"  src="../assets/img/3.webp" alt="item">
+            <img class="second-img" src="../assets/img/3b.webp" alt="item">
+          </div>
+          <div class="discount">
+            <span class="discount-red">-50%</span>
+          </div>
+          <div class="heart">
+            <span>&hearts;</span>
+          </div>
+        </div>
+        <div class="text">
+          <span>Come zucchero filato</span>
+          <span>VOGLIA DI COLORI PASTELLO</span>
+        </div>
+        <div class="prezzo">
+          <span>129,99 &euro;</span>
+          <span class="line-through">184,99 &euro;</span>
+        </div>
+      </div>
+      <div class="card">
+        <div class="contenuto-card">
+          <div class="image-card">
+            <img class="first-img"  src="../assets/img/4.webp" alt="item">
+            <img class="second-img" src="../assets/img/4b.webp" alt="item">
+          </div>
+        <div class="discount">
+          <span class="discount-red">-50%</span>
+          <span class=" discount-green">Sostenibilità</span>
+        </div>
+        <div class="heart">
+          <span>&hearts;</span>
+        </div>
+        </div>
+        <div class="text">
+          <span>Levi's</span>
+          <span>TEE UNISEX</span>
+        </div>
+        <div class="prezzo">
+          <span>14,99 &euro;</span>
+          <span class="line-through">29,99 &euro;</span>
+        </div>
+      </div>
+      <div class="card">
+        <div class="contenuto-card">
+          <div class="image-card">
+            <img class="first-img"  src="../assets/img/5.webp" alt="item">
+            <img class="second-img" src="../assets/img/5b.webp" alt="item">
+          </div>
+          <div class="heart">
+            <span>&hearts;</span>
+          </div>
+        </div>
+        <div class="text">
+          <span>Maya Deluxe</span>
+          <span>STRIPE BODICE</span>
+        </div>
+        <div class="prezzo">
+          <span>99,99 &euro;</span>
+        </div>
+      </div>
+      <div class="card">
+        <div class="contenuto-card">
+          <div class="image-card">
+            <img class="first-img"  src="../assets/img/6.webp" alt="item">
+            <img class="second-img" src="../assets/img/6b.webp" alt="item">
+          </div>
+          <div class="discount">
+            <span class=" discount-green">Sostenibilità</span>
+          </div>
+          <div class="heart">
+            <span>&hearts;</span>
+          </div>
+        </div>
+        <div class="text">
+          <span>Espirit</span>
+          <span>MAGLIONE-BLACK</span>
+        </div>
+        <div class="prezzo">
+          <span>29,99 &euro;</span>
+        </div>
+      </div>
+
+      
+    </div>
+  </main>
 </template>
 
 <style>
