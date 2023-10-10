2573246f261b369850321c9521d0642639aefbd8
Alethepata <alessia.patane@gmail.com>
2023-10-10 15:33:51 +0200

    [Add] header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,29 @@ export default {
 </script>
 
 <template>
-  
+      <header>
+    <div class="topbar">
+      <div class="container flex">
+        <nav>
+          <ul class="flex">
+            <li><a href="#">Donna</a></li>
+            <li><a href="#">Uomo</a></li>
+            <li><a href="#">Bambini</a></li>
+          </ul>
+        </nav>
+
+        <div class="logo">        
+          <img src="../../public/boolean-logo.png" alt="logo">
+        </div>
+
+        <nav class="flex">
+          <a href="#"><i class="fa-regular fa-user"></i></a>
+          <a href="#"><i class="fa-regular fa-heart"></i></a>
+          <a href="#"><i class="fa-solid fa-bag-shopping"></i></a>
+        </nav>
+      </div>
+    </div>
+  </header>
 </template>
 
 <style>
