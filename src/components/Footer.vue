8c631216ab86f117a8a7e75637f78c71f232917e
Alethepata <alessia.patane@gmail.com>
2023-10-10 15:34:27 +0200

    [Add] footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,30 @@ export default {
 </script>
 
 <template>
-  
+    <footer>
+       <div class="container">
+         <div class="second-text float">
+           <h4>Boolando s.r.l</h4>
+           <nav>
+             <ul class="flex">
+               <li><a href="#">Informazioni legali</a></li>
+               <li><a href="#">Informativa sulla privacy</a></li>
+               <li><a href="#">Diritto di recesso</a></li>
+             </ul>
+           </nav>
+         </div>
+         <div class="social">
+           <h4>Trovaci anche su</h4>
+           <nav>
+             <a href="#"><i class="fa-brands fa-square-twitter"></i></a>
+             <a href="#"><i class="fa-brands fa-square-facebook"></i></a>
+             <a href="#"><i class="fa-brands fa-square-instagram"></i></a>
+             <a href="#"><i class="fa-brands fa-square-pinterest"></i></a>
+             <a href="#"><i class="fa-brands fa-square-youtube"></i></a>
+           </nav>
+         </div>
+       </div>
+    </footer>
 </template>
 
 <style>
