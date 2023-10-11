563560c035d408e5fa3dca32013b2e59cbcb7205
Alethepata <alessia.patane@gmail.com>
2023-10-11 15:42:01 +0200

    [Add] menu

---
+++ src/components/partials/Menu.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'Menu',
+    props: {
+        type: String,
+        link: String
+    }
+
+}
+</script>
+
+<template>
+
+            <li><a :href="link">{{type}}</a></li>
+  
+</template>
+
+<style>
+
+</style>
