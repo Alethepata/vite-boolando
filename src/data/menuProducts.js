431b5a1de5988abcd8796096cf3c97b75d126a9f
Alethepata <alessia.patane@gmail.com>
2023-10-11 15:41:42 +0200

    [Add] menuProducts

---
+++ src/data/menuProducts.js
@@ -0,0 +1,15 @@
+export default [
+    {
+        type: 'Uomo',
+        link: '#'
+    },
+    {
+        type: 'Donna',
+        link: '#'
+    },
+    {
+        type: 'Bambino',
+        link: '#'
+    }
+    
+]
