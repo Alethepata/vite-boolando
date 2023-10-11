7188608e33358776427a637814ea3eb78cf32626
Alethepata <alessia.patane@gmail.com>
2023-10-11 14:33:27 +0200

    [Add] products

---
+++ src/data/products.js
@@ -0,0 +1,93 @@
+export default [
+    
+    {
+        brand: 'Levi\'s',
+        description: 'RELAXED FIT TEE UNISEX',
+        discount: '-50%', 
+        sustainability: true,
+        price: '€ 29,99',
+        discountedPrice: '€ 14,99',
+        images: [
+          {
+            first: '1.webp',
+            second: '1b.webp'
+          }
+        ]
+      },
+      {
+        brand: 'Guess',
+        description: 'ROSES TEE',
+        discount: '-50%', 
+        sustainability: false,
+        price: '€ 29,99',
+        discountedPrice: '€ 20,99',
+        images: [
+          {
+            first: '2.webp',
+            second: '2b.webp'
+          }
+        ]
+
+      },
+      {
+        brand: 'Come zucchero filato',
+        description: 'VOGLIA DI COLORI PASTELLO',
+        discount: '-50%', 
+        sustainability: false,
+        price: '€ 184,99',
+        discountedPrice: '€ 129,99',
+        images: [
+          {
+            first: '3.webp',
+            second: '3b.webp'
+          }
+        ]
+
+      },
+      {
+        brand: 'Levi\'s',
+        description: 'TEE UNISEX',
+        discount: '-50%', 
+        sustainability: true,
+        price: '€ 29,99',
+        discountedPrice: '€ 14,99',
+        images: [
+          {
+            first: '4.webp',
+            second: '4b.webp'
+          }
+        ]
+
+      },
+      {
+        brand: 'Maya Deluxe',
+        description: 'STRIPE BODICE',
+        discount: null, 
+        sustainability: false,
+        price: '€ 99,99',
+        discountedPrice: null,
+        images: [
+          {
+            first: '5.webp',
+            second: '5b.webp'
+          }
+        ]
+
+      },
+      {
+        brand: 'Espirit',
+        description: 'MAGLIONE-BLACK',
+        discount: null, 
+        sustainability: true,
+        price: '€29,99',
+        discountedPrice: null,
+        images: [
+          {
+            first: '6.webp',
+            second: '6b.webp'
+          }
+        ]
+
+      }
+    
+]
