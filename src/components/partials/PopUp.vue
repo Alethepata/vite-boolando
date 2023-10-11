6654e0b48754f6c753710cc96dbf05f18ab6a900
Alethepata <alessia.patane@gmail.com>
2023-10-11 21:42:48 +0200

    [Add] pop up

---
+++ src/components/partials/PopUp.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    name: 'Pop-up',
+    props: {
+        popUp: Object,
+        clicked: String
+    }
+}
+</script>
+
+<template>
+  <div class="pop-up">
+       <h1>{{ popUp.brand }}</h1>  
+       <p>{{ popUp.story }}</p> 
+  </div>
+</template>
+
+<style lang="scss">
+@use '../../scss/partials/variabiles' as *;
+
+.pop-up{
+    position: absolute;
+    top:200px;
+    left: 50%;
+    transform: translate(-50%);
+    z-index: 800;
+    background-color: $white;
+    border:1px solid $grey;
+    padding: 40px;
+    border-radius: 30px;
+}
+
+</style>
